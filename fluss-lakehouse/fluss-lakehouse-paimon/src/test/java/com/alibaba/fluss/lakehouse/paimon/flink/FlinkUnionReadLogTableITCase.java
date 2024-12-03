efbecbff90385b97329f555ed71440406655d112
luoyuxia <luoyuxia.luoyuxia@alibaba-inc.com>
2024-12-03 20:19:48 +0800

    do some fix

--- fluss-lakehouse/fluss-lakehouse-paimon/src/test/java/com/alibaba/fluss/lakehouse/paimon/flink/FlinkUnionReadLogTableITCase.java
+++ fluss-lakehouse/fluss-lakehouse-paimon/src/test/java/com/alibaba/fluss/lakehouse/paimon/flink/FlinkUnionReadLogTableITCase.java
@@ -62,9 +62,7 @@ class FlinkUnionReadLogTableITCase extends FlinkUnionReadTestBase {
         List<Row> writtenRows = new ArrayList<>();
         long tableId = prepareLogTable(t1, DEFAULT_BUCKET_NUM, isPartitioned, writtenRows);
         // wait until records has has been synced
-        System.out.println("waitUtilBucketSynced");
         waitUtilBucketSynced(t1, tableId, DEFAULT_BUCKET_NUM, isPartitioned);
-        System.out.println("finish waitUtilBucketSynced");
 
         // now, start to read the log table, which will read paimon
         // may read fluss or not, depends on the log offset of paimon snapshot
