4c3d71420996c8abdbf98fd6d6abced6251e0eef
luoyuxia <luoyuxia.luoyuxia@alibaba-inc.com>
2024-12-20 15:15:45 +0800

    fix out of order issue

--- fluss-server/src/main/java/com/alibaba/fluss/server/log/LogTablet.java
+++ fluss-server/src/main/java/com/alibaba/fluss/server/log/LogTablet.java
@@ -634,14 +634,18 @@ public final class LogTablet {
                 appendInfo.setStartOffsetOfMaxTimestamp(startOffset);
                 appendInfo.setDuplicated(true);
             } else {
-                // Append the records, and increment the local log end offset immediately after
-                // append because write to the transaction index below may fail, and we want to
-                // ensure that the offsets of future appends still grow monotonically.
-                localLog.append(
-                        appendInfo.lastOffset(),
-                        appendInfo.maxTimestamp(),
-                        appendInfo.startOffsetOfMaxTimestamp(),
-                        validRecords);
+                // if there are records to append
+                if (appendInfo.lastOffset() >= appendInfo.firstOffset()) {
+                    // Append the records, and increment the local log end offset immediately after
+                    // append because write to the transaction index below may fail, and we want to
+                    // ensure that the offsets of future appends still grow monotonically.
+                    localLog.append(
+                            appendInfo.lastOffset(),
+                            appendInfo.maxTimestamp(),
+                            appendInfo.startOffsetOfMaxTimestamp(),
+                            validRecords);
+                }
+
                 updateHighWatermarkWithLogEndOffset();
 
                 // update the writer state.
