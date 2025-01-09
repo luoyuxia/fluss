21d15ed8d872567308a5c03687dc03d8c7c0bda5
luoyuxia <luoyuxia.luoyuxia@alibaba-inc.com>
2025-01-09 14:37:29 +0800

    fix compile error

--- fluss-common/src/main/java/com/alibaba/fluss/config/ConfigOptions.java
+++ fluss-common/src/main/java/com/alibaba/fluss/config/ConfigOptions.java
@@ -1370,20 +1370,4 @@ public class ConfigOptions {
         LZ4,
         ZSTD
     }
-
-    /** The merge engine for primary key table. */
-    public enum MergeEngine {
-        FIRST_ROW("first_row");
-
-        private final String value;
-
-        MergeEngine(String value) {
-            this.value = value;
-        }
-
-        @Override
-        public String toString() {
-            return value;
-        }
-    }
 }
