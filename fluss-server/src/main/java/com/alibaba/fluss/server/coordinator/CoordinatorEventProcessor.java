33facf0e8b96a793ecb717a07affad2c855dcf4d
luoyuxia <luoyuxia.luoyuxia@alibaba-inc.com>
2025-03-25 17:58:17 +0800

    [coordinator] Kick off offline replica from bucket isr

--- fluss-server/src/main/java/com/alibaba/fluss/server/coordinator/CoordinatorEventProcessor.java
+++ fluss-server/src/main/java/com/alibaba/fluss/server/coordinator/CoordinatorEventProcessor.java
@@ -184,7 +184,8 @@ public class CoordinatorEventProcessor implements EventProcessor {
                 new ReplicaStateMachine(
                         coordinatorContext,
                         new CoordinatorRequestBatch(
-                                coordinatorChannelManager, coordinatorEventManager));
+                                coordinatorChannelManager, coordinatorEventManager),
+                        zooKeeperClient);
         this.tableBucketStateMachine =
                 new TableBucketStateMachine(
                         coordinatorContext,
@@ -651,7 +652,6 @@ public class CoordinatorEventProcessor implements EventProcessor {
     }
 
     private void onReplicaBecomeOffline(Set<TableBucketReplica> offlineReplicas) {
-
         LOG.info("The replica {} become offline.", offlineReplicas);
         for (TableBucketReplica offlineReplica : offlineReplicas) {
             coordinatorContext.addOfflineBucketInServer(
--- fluss-server/src/main/java/com/alibaba/fluss/server/zk/data/LeaderAndIsr.java
+++ fluss-server/src/main/java/com/alibaba/fluss/server/zk/data/LeaderAndIsr.java
@@ -69,6 +69,10 @@ public class LeaderAndIsr {
         this.bucketEpoch = bucketEpoch;
     }
 
+    public LeaderAndIsr newLeaderAndIsr(List<Integer> newIsr) {
+        return new LeaderAndIsr(leader, leaderEpoch, newIsr, coordinatorEpoch, bucketEpoch + 1);
+    }
+
     public int leader() {
         return leader;
     }
