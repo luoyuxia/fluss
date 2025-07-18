47dc2c9616f882b4df39f39909a09cf6740affe6
Hongshun Wang <loserwang1024@gmail.com>
2025-07-18 17:16:22 +0800

    Revert "ban creating log table"
    This reverts commit b1552d248d33b4f956a2034e5275f5e75dc09256.

--- fluss-server/src/main/java/com/alibaba/fluss/server/coordinator/CoordinatorService.java
+++ fluss-server/src/main/java/com/alibaba/fluss/server/coordinator/CoordinatorService.java
@@ -247,12 +247,6 @@ public final class CoordinatorService extends RpcServiceBase implements Coordina
         // apply system defaults if the config is not set
         tableDescriptor = applySystemDefaults(tableDescriptor);
 
-        if (!tableDescriptor.hasPrimaryKey()) {
-            throw new InvalidTableException(
-                    "Currently, Only primary-key table is support in this cluster. if you want "
-                            + "use log table, please contact 温粥(263086) for help.");
-        }
-
         // the distribution and bucket count must be set now
         //noinspection OptionalGetWithoutIsPresent
         int bucketCount = tableDescriptor.getTableDistribution().get().getBucketCount().get();
