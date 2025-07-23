3a2d9d3451a4b7f7c4d4e384fd1837728a1ccabf
luoyuxia <luoyuxia@alumni.sjtu.edu.cn>
2025-07-23 20:43:34 +0800

    add alake rest catalog

--- fluss-lake/fluss-lake-paimon/src/main/java/com/alibaba/fluss/lake/paimon/PaimonLakeCatalog.java
+++ fluss-lake/fluss-lake-paimon/src/main/java/com/alibaba/fluss/lake/paimon/PaimonLakeCatalog.java
@@ -26,15 +26,18 @@ import com.alibaba.fluss.metadata.TableDescriptor;
 import com.alibaba.fluss.metadata.TablePath;
 import com.alibaba.fluss.utils.IOUtils;
 
+import com.alibaba.alake.common.options.AlakeOptionsBuilder;
+import com.alibaba.alake.core.builder.check.trace.model.EngineType;
 import org.apache.paimon.CoreOptions;
 import org.apache.paimon.catalog.Catalog;
 import org.apache.paimon.catalog.CatalogContext;
-import org.apache.paimon.catalog.CatalogFactory;
 import org.apache.paimon.catalog.Identifier;
 import org.apache.paimon.options.Options;
 import org.apache.paimon.schema.Schema;
 import org.apache.paimon.types.DataType;
 import org.apache.paimon.types.DataTypes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -46,6 +49,8 @@ import static com.alibaba.fluss.metadata.TableDescriptor.TIMESTAMP_COLUMN_NAME;
 
 /** A Paimon implementation of {@link LakeCatalog}. */
 public class PaimonLakeCatalog implements LakeCatalog {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PaimonLakeCatalog.class);
 
     private static final LinkedHashMap<String, DataType> SYSTEM_COLUMNS = new LinkedHashMap<>();
 
@@ -68,9 +73,27 @@ public class PaimonLakeCatalog implements LakeCatalog {
     private static final String PAIMON_CONF_PREFIX = "paimon.";
 
     public PaimonLakeCatalog(Configuration configuration) {
-        this.paimonCatalog =
-                CatalogFactory.createCatalog(
-                        CatalogContext.create(Options.fromMap(configuration.toMap())));
+        Options catalogOptions = Options.fromMap(configuration.toMap());
+        catalogOptions.set("metastore", "paimon-alake-rest");
+        // catalogOptions.set("dlf.catalog.id", "alake");
+        // 访问的DLF的region地址，目前弹内的DLF暂时只部署到了张家口，所以先写死，后续有多region的时候再升级
+        catalogOptions.set("dlf.catalog.region", "cn-zhangjiakou");
+
+        catalogOptions.set("warehouse", "alake");
+        catalogOptions.set("dlf.deploy.env", "prod");
+        // 引擎类型，不同引擎使用不同的值
+        // 具体枚举参考com.alibaba.alake.core.buidler.check.trace.model.EngineType
+        catalogOptions.set("dlf.request.engine", EngineType.ALAKE.name());
+
+        catalogOptions.set("dlf.catalog.endpoint", "metastore-inner.aliyuncs.com");
+
+        LOG.info("catalog options: {}", catalogOptions);
+
+        CatalogContext catalogContext =
+                CatalogContext.create(
+                        AlakeOptionsBuilder.create(catalogOptions)
+                                .build("cn-zhangjiakou@AY@306527"));
+        this.paimonCatalog = new com.alibaba.alake.rest.catalog.AlakeRestCatalog(catalogContext);
     }
 
     @Override
